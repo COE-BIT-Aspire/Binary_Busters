2df65974929037bca7fedb15951d59676a3dc2fe
V-SOWMIYA <84324454+V-SOWMIYA@users.noreply.github.com>
2024-04-30 10:50:09 +0530

    Add files via upload

--- Sowmiya/js/demo.js
+++ Sowmiya/js/demo.js
@@ -32,7 +32,7 @@ const inventory = [
     { item: "Chair", quantity: 30 },
 ];
 
-// Function to calculate total quantity of a specific item
+
 function getTotalQuantity(itemName, items) {
     let totalQuantity = 0;
     for (const item of items) {
@@ -43,7 +43,6 @@ function getTotalQuantity(itemName, items) {
     return totalQuantity;
 }
 
-// Get the total quantity of "Chair" items in the inventory
 const chairQuantity = getTotalQuantity("Chair", inventory);
 console.log("Total Chair Quantity:", chairQuantity);
 
@@ -52,31 +51,32 @@ console.log("Total Chair Quantity:", chairQuantity);
 
 
 
-// Define a set to store unique employee IDs of those who attended a meeting
+
 const meetingAttendees = new Set();
 
-// Function to add an employee ID to the set of attendees
+
 function markAttendance(employeeId) {
     meetingAttendees.add(employeeId);
 }
 
-// Function to check if an employee has attended the meeting
+
 function hasAttended(employeeId) {
     return meetingAttendees.has(employeeId);
 }
 
-// Mark attendance for employees
 markAttendance("emp123");
 markAttendance("emp456");
 markAttendance("emp123");
 
-// Check if specific employees have attended the meeting
+
 console.log("Employee with ID emp123 has attended the meeting:", hasAttended("emp123"));
 console.log("Employee with ID emp789 has attended the meeting:", hasAttended("emp789"));
 
 
 
 // Array of product objects
+
+
 const products = [
     { name: "Laptop", price: 1200 },
     { name: "Smartphone", price: 800 },
@@ -85,17 +85,7 @@ const products = [
     { name: "Smartwatch", price: 250 },
 ];
 
-// Function to filter products within a price range
-const filterProductsByPriceRange = (products, minPrice, maxPrice) => {
-    return products.filter(product => product.price >= minPrice && product.price <= maxPrice);
-};
 
-// Filter products between $200 and $500
-const filteredProducts = filterProductsByPriceRange(products, 200, 500);
-
-// Display filtered products
-console.log("Products between $200 and $500:");
-filteredProducts.forEach(product => console.log(`${product.name}: $${product.price}`));
 
 // closure 
 
